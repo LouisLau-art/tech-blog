318600ba8ad783276ef8cd218970d34b40af05f5
LouisLau-art <louis.shawn@qq.com>
2025-12-15 16:37:21 +0800

    chore: update application constants

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Louis的技术博客';
+export const SITE_DESCRIPTION = '分享技术心得与学习笔记';
