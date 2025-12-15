442e0985c522c5d9fb727b154d57554764629e3f
LouisLau-art <louis.shawn@qq.com>
2025-12-15 16:05:36 +0800

    feat: Implement LaTeX rendering, add new blog post, and enhance blog post layout with a width toggle and updated social links.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,16 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://LouisLau-art.github.io',
 	base: '/tech-blog',
 	integrations: [mdx(), sitemap()],
+	markdown: {
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex],
+	},
 });
-
